b6513614ff6cf7daadae075b914e2bcba4375343
Nihar Koli <42617538+nihar-koli@users.noreply.github.com>
2019-05-01 17:30:54 +0530

    Added pattern printing program

---
+++ PATTERN5.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int i,j;
+
+  clrscr();
+
+    printf("\n\n");
+
+    for(i=1;i<=5;i++)
+    {
+       for(j=1;j<=i;j++)
+       {
+	  printf(" %d ",j);
+       }
+       printf("\n");
+
+    }
+
+  getch();
+}
+
